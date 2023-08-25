5ed2777dc74477ea895b93e166024be296c8f306
alireza <alirezaarzehgar82@gmail.com>
2023-08-25 18:25:34 +0330

    models: create order mechanism
    - Add json tags to Order model.
    - Implement `CreateOrder()` function.
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/orders.go
+++ models/orders.go
@@ -1,11 +1,19 @@
 package models
 
 type Order struct {
-	ID          uint `gorm:"primaryKey"`
-	UserID      uint
-	Status      string  `gorm:"not null"`
-	Description string  `gorm:"not null"`
-	Price       float64 `gorm:"not null"`
+	ID          uint   `gorm:"primaryKey" json:"id"`
+	UserID      uint   `json:"user_id"`
+	Status      string `gorm:"not null;default:processing" json:"status"`
+	Description string `gorm:"not null" json:"description"`
 
-	User User
+	User User `json:"-"`
+}
+
+func CreateOrder(userId uint, description string) (uint, error) {
+	order := Order{
+		UserID:      userId,
+		Description: description,
+	}
+	err := db.Create(&order).Error
+	return order.ID, err
 }
