cc57facf384e6fcc156e016c502d6de9d567903c
alireza <alirezaarzehgar82@gmail.com>
2023-08-25 18:24:18 +0330

    controllers: implement create order
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- controllers/orders.go
+++ controllers/orders.go
@@ -1,1 +1,42 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/BaseMax/RabbitMQOrderGo/broker"
+	"github.com/BaseMax/RabbitMQOrderGo/helpers"
+	"github.com/BaseMax/RabbitMQOrderGo/models"
+)
+
+func CreateOrder(c echo.Context) error {
+	var order models.Order
+	if err := json.NewDecoder(c.Request().Body).Decode(&order); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	iid, _ := helpers.GetLoggedinUserInfo(c)
+	orderId, err := models.CreateOrder(iid, order.Description)
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+
+	order = models.Order{
+		ID:          orderId,
+		UserID:      iid,
+		Description: order.Description,
+		Status:      "processing",
+	}
+
+	if broker.IsClosed() {
+		if broker.ConnectAndCreateQueue() != nil {
+			return echo.ErrInternalServerError
+		}
+	}
+	if broker.EnqueueOrderToRabbit(order) != nil {
+		return echo.ErrInternalServerError
+	}
+	return c.JSON(http.StatusOK, order)
+}
