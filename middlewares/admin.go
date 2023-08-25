ab6dd0d867aa453e80f62c49d5d4726c299f81cd
alireza <alirezaarzehgar82@gmail.com>
2023-08-25 15:42:41 +0330

    middlewares: implement IsAdmin
    * Just check username to configured admin name.
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- middlewares/admin.go
+++ middlewares/admin.go
@@ -1,9 +1,25 @@
 package middlewares
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+
+	"github.com/BaseMax/RabbitMQOrderGo/conf"
+)
 
 func IsAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 	return echo.HandlerFunc(func(c echo.Context) error {
+		bearer := c.Request().Header.Get("Authorization")
+		if bearer == "" {
+			return echo.ErrBadRequest
+		}
+		token, _, _ := new(jwt.Parser).ParseUnverified(bearer[len("Bearer "):], jwt.MapClaims{})
+		claims := token.Claims.(jwt.MapClaims)
+
+		name, _, _ := conf.GetAdminInfo()
+		if claims["iss"] != name {
+			return echo.ErrUnauthorized
+		}
 		return next(c)
 	})
 }
