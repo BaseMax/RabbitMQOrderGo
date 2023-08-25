76d922c79d69c815a6a013cf10529466596cda69
alireza <alirezaarzehgar82@gmail.com>
2023-08-25 20:57:05 +0330

    controllers: implement some order functions
    Just non-admin priviledged controlers:
    - `FetchOrder()`
    - `EditOrder()`
    - `OrderStatus()`
    - `CancelOrder()`
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- controllers/orders.go
+++ controllers/orders.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 
@@ -42,17 +43,51 @@ func CreateOrder(c echo.Context) error {
 }
 
 func FetchOrder(c echo.Context) error {
-	return nil
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+	order, err := models.GetOrderById(uint(id))
+	if err != nil {
+		return echo.ErrNotFound
+	}
+	return c.JSON(http.StatusOK, order)
 }
 
 func EditOrder(c echo.Context) error {
-	return nil
+	var order models.Order
+	if err := json.NewDecoder(c.Request().Body).Decode(&order); err != nil {
+		return echo.ErrBadRequest
+	}
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+	if models.UpdateOrder(uint(id), order.Description, "") != nil {
+		return echo.ErrNotFound
+	}
+	return c.NoContent(http.StatusNoContent)
 }
 
 func OrderStatus(c echo.Context) error {
-	return nil
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+	order, err := models.GetOrderById(uint(id))
+	if err != nil {
+		return echo.ErrNotFound
+	}
+	return c.JSON(http.StatusOK, map[string]any{"status": order.Status})
 }
 
 func CancelOrder(c echo.Context) error {
-	return nil
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+	if models.UpdateOrder(uint(id), "", "canceled") != nil {
+		return echo.ErrNotFound
+	}
+	return c.NoContent(http.StatusNoContent)
 }
