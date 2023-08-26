66f7a2a1a25b2cfc17b7d8fa292252421d20baee
alireza <alirezaarzehgar82@gmail.com>
2023-08-26 04:44:45 +0330

    models: check order status on `CreateRefund()`
    - Add constant for errors.
    - Omit Order from model json.
    - Set OrderID not null.
    - Implement mechanism to avoid creating refund without completed status.
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/refunds.go
+++ models/refunds.go
@@ -1,20 +1,32 @@
 package models
 
+import "errors"
+
 const (
 	REFUND_STATUS_APPENDING = "appending"
 	REFUND_STATUS_APPROVED  = "approved"
 	REFUND_STATUS_CANCEL    = "cancel"
+
+	REFUND_ERROR_BADORDER = "Order is not completed"
 )
 
 type Refund struct {
 	ID      uint   `gorm:"primaryKey" json:"id"`
-	OrderID uint   `gorm:"unique" json:"order_id"`
+	OrderID uint   `gorm:"not null;unique" json:"order_id"`
 	Status  string `gorm:"not null;default:appending" json:"status"`
 
-	Order Order `json:"order"`
+	Order Order `json:"-"`
 }
 
 func CreateRefund(orderId uint) (id uint, err error) {
+	order, err := GetOrderById(orderId)
+	if err != nil {
+		return 0, err
+	}
+	if order.Status != ORDER_STATUS_COMPLETED {
+		return 0, errors.New(REFUND_ERROR_BADORDER)
+	}
+
 	Refund := Refund{
 		OrderID: orderId,
 		Status:  REFUND_STATUS_APPENDING,
