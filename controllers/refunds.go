047922fb135384dbab96b6a6615b02bd7cca338c
alireza <alirezaarzehgar82@gmail.com>
2023-08-26 03:55:54 +0330

    controllers: add all prototypes
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- controllers/refunds.go
+++ controllers/refunds.go
@@ -1,1 +1,35 @@
 package controllers
+
+import "github.com/labstack/echo/v4"
+
+func CreateRefund(c echo.Context) error {
+	return nil
+}
+
+func FetchRefund(c echo.Context) error {
+	return nil
+}
+
+func FetchAllRefunds(c echo.Context) error {
+	return nil
+}
+
+func RefundStatus(c echo.Context) error {
+	return nil
+}
+
+func CancelRefund(c echo.Context) error {
+	return nil
+}
+
+func FirstRefund(c echo.Context) error {
+	return nil
+}
+
+func CompleteRefund(c echo.Context) error {
+	return nil
+}
+
+func DeleteRefund(c echo.Context) error {
+	return nil
+}
