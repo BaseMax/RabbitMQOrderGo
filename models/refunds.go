f2ed3d88f999b8f82c4676c94739cfa41967b0fb
alireza <alirezaarzehgar82@gmail.com>
2023-08-26 03:55:27 +0330

    models: implement refund Note: I just copy and paste orders to refund model. Currently it's very error prone.
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/refunds.go
+++ models/refunds.go
@@ -1,8 +1,43 @@
 package models
 
+const (
+	REFUND_STATUS_APPENDING = "appending"
+	REFUND_STATUS_APPROVED  = "approved"
+	REFUND_STATUS_CANCEL    = "cancel"
+)
+
 type Refund struct {
-	ID      uint `gorm:"primaryKey"`
-	OrderID uint `gorm:"unique"`
-	Order   Order
-	Status  string `gorm:"not null"`
+	ID      uint   `gorm:"primaryKey" json:"id"`
+	OrderID uint   `gorm:"unique" json:"order_id"`
+	Status  string `gorm:"not null;default:appending" json:"status"`
+
+	Order Order `json:"order"`
+}
+
+func CreateRefund(orderId uint) (id uint, err error) {
+	Refund := Refund{
+		OrderID: orderId,
+		Status:  REFUND_STATUS_APPENDING,
+	}
+	err = db.Create(&Refund).Error
+	id = Refund.ID
+	return
+}
+
+func GetRefundById(id uint) (Refund Refund, err error) {
+	err = db.First(&Refund, id).Error
+	return
+}
+
+func GetAllRefunds() (Refunds []Refund, err error) {
+	err = db.Find(&Refunds).Error
+	return
+}
+
+func UpdateRefund(id uint, status string) int64 {
+	return db.Where(id).Updates(Refund{Status: status}).RowsAffected
+}
+
+func DeleteRefund(id uint) int64 {
+	return db.Delete(&Refund{}, id).RowsAffected
 }
